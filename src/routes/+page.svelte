fe34b721d95c32f6b9c9f213fd3681017eac6861
ccalobeto <calobeto.leon@gmail.com>
2024-11-25 11:09:25 -0500

    build main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
+<script lang="ts">
+	import Donut from '$lib/components/Donut.svelte';
+	import type { PageData } from './$types';
+	import type Data from '$lib/types';
+
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
+
+	let missed: Data[] = data.missed.map((d) => ({ ...d, value: +d.value }));
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Donut data={missed} />
