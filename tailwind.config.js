9cb6c8f62229e237281037e7c77961cf66fd801e
ccalobeto <calobeto.leon@gmail.com>
2024-12-10 14:36:25 -0500

    setup layerchart

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,20 @@ export default {
   content: [
     './src/**/*.{html,js,svelte,ts}',
     './node_modules/svelte-ux/**/*.{svelte,ts}',
+    './node_modules/layerchart/**/*.{svelte,ts}'
   ],
   theme: {
     extend: {
+      colors:{
+        // LayerChart tokens mapped to shadcn-svelte colors.
+				surface: {
+					content: "hsl(var(--card-foreground) / <alpha-value>)",
+					100: "hsl(var(--background) / <alpha-value>)",
+					200: "hsl(var(---muted) / <alpha-value>)",
+					// not sure what color maps here (should be darker than 200).  Could add a new color to `app.css`
+					300: "hsl(var(--background) / <alpha-value>)"
+				},
+      }
   },
   plugins: [],
 }
